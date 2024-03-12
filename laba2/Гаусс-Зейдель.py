8ae41251db0821b62aa05e43aaafbe0fdc9706df
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-03-12 18:31:05 +0600

    Update Гаусс-Зейдель.py

--- laba2/Гаусс-Зейдель.py
+++ laba2/Гаусс-Зейдель.py
@@ -1,22 +1,25 @@
 import numpy as np
-
 from scipy.optimize import minimize_scalar
-A=10
-a=3
-b=2
 
-eps=1e-5
-x_0=np.array([0,0])
-k=1
-j=1
+A = 10
+a = 3
+b = 2
+
+def f(x):
+    return A - (x[0]-a)*np.exp(-(x[0]-a)) - (x[1]-b)*np.exp(-(x[1]-b))
+
+eps = 1e-5
+x_0 = np.array([0, 0])
+k = 1
+j = 1
 y = np.copy(x_0)
-n=2 
-f = lambda x: A - (x[0]-a)*np.exp(-(x[0]-a)) - (x[1]-b)*np.exp(-(x[1]-b))
-flag=True 
+n = 2 
+flag = True 
+
 while flag:
     result = minimize_scalar(lambda lmbd: f(y + lmbd * np.eye(n)[j-1]))
     lmbd = result.x
-     # Обновление y
+    
     y = y + lmbd * np.eye(n)[j-1]
 
     if j < n:
