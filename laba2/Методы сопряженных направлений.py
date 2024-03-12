33f0241989e16733eb839a905f50a424199ef5d9
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-03-12 19:35:24 +0600

    Create Методы сопряженных направлений.py

---
+++ laba2/Методы сопряженных направлений.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.optimize import minimize
+
+A = 10
+a = 3
+b = 2
+
+def f(x):
+    return A - (x[0]-a)*np.exp(-(x[0]-a)) - (x[1]-b)*np.exp(-(x[1]-b))
+
+x = np.array([0.0, 0.0])
+
+eps = 1e-6
+
+# Сопряженные направления
+S = np.eye(2)
+flag = True
+
+while flag:
+    x_old = x.copy()
+    for i in range(2):
+        result = minimize(lambda lam: f(x + lam * S[:, i]), 0)
+        x = x + result.x * S[:, i]
+
+    y = x - x_old
+    if np.linalg.norm(y) < eps:
+        break
+
+    # Обновление сопряженных направлений
+    S = np.roll(S, -1, axis=1)
+    S[:, -1] = y / np.linalg.norm(y)
+
+print(x)
+#[3.9999883  2.99998176]
