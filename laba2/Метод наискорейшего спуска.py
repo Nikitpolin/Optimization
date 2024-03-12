67167bcd127000574ec4f69190e4483f71f66997
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-03-12 19:37:08 +0600

    Create Метод наискорейшего спуска.py

---
+++ laba2/Метод наискорейшего спуска.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.optimize import minimize_scalar
+
+A = 10
+a = 3
+b = 2
+
+def f(x):
+    return A - (x[0]-a)*np.exp(-(x[0]-a)) - (x[1]-b)*np.exp(-(x[1]-b))
+
+flag = True
+def grad_f(x):
+    dfdx = (x[0]-a)*np.exp(-(x[0]-a)) + 1
+    dfdy = (x[1]-b)*np.exp(-(x[1]-b)) + 1
+    return np.array([dfdx, dfdy])
+
+x = np.array([0.0, 0.0])
+eps = 1e-6
+
+while flag:
+    g = grad_f(x)
+
+    if np.linalg.norm(g) < eps:
+        break
+
+    # направление спуска
+    S = -g / np.linalg.norm(g)
+
+    res = minimize_scalar(lambda alpha: f(x + alpha*S))
+    alpha_opt = res.x
+
+
+    x = x + alpha_opt * S
+
+print(x)
+#[3.9999883  2.99998176]
