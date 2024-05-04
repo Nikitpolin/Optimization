794681c301ffd25688ccd5f6264b8b85340331a1
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-05-04 08:12:50 +0600

    Update метод последовательных уступок.py

--- laba5/метод последовательных уступок.py
+++ laba5/метод последовательных уступок.py
@@ -2,40 +2,40 @@ import numpy as np
 
 # Задание функций критериев fi(x)
 def f1(x):
-    return a1 * (x[0] - b1)**2 + c1 * (x[1] - d1)**2
+    return (x[0] - 2)**2 + 3 * (x[1] - 4)**2
 
 def f2(x):
-    return a2 * (x[0] - b2)**2 + c2 * (x[1] - d2)**2
+    return 2 * (x[0] + 3)**2 + 4 * (x[1] + 1)**2
 
 def f3(x):
-    return a3 * (x[0] - b3)**2 + c3 * (x[1] - d3)**2
+    return 3 * (x[0] + 4)**2 + 5 * (x[1] - 3)**2
 
 # Задание параметров функций
-a1, b1, c1, d1 = 1, 2, 3, 4
-a2, b2, c2, d2 = 2, -3, 4, -1
-a3, b3, c3, d3 = 3, 4, 5, -3
+a1, a2, a3 = 0.4, 0.3, 0.3
 
-# Реализация метода последовательных уступок
-def successive_concessions(x):
-    f_values = [f1(x), f2(x), f3(x)]
+# Реализация метода последовательных уступок с весами
+def sequential_yield(x):
+    f_values = [a1 * f1(x), a2 * f2(x), a3 * f3(x)]
     min_index = np.argmin(f_values)
+    min_value = min(f_values)
+    for i, f in enumerate(f_values):
+        if f < min_value:
+            return None
     return min_index + 1
 
 # Поиск оптимальной точки
 def find_optimal_point():
-    # Инициализация оптимальной точки и минимального индекса
+    # Инициализация оптимальной точки
     optimal_point = None
-    min_index = np.inf
     
     # Перебор всех возможных точек
     for x1 in np.linspace(0, 10, num=100):
         for x2 in np.linspace(0, 10, num=100):
             # Вычисление индекса минимальной функции
-            index = successive_concessions((x1, x2))
-            # Поиск минимального индекса
-            if index < min_index:
-                min_index = index
+            index = sequential_yield((x1, x2))
+            if index is not None:
                 optimal_point = (x1, x2)
+                return optimal_point
     
     return optimal_point if optimal_point is not None else "No optimal point found"
 
