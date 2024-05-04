f0cd68c86e239f0d58635c71bb9f7a9829a1543b
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-05-04 08:11:54 +0600

    Update Метод дискриминационного анализа.py

--- laba5/Метод дискриминационного анализа.py
+++ laba5/Метод дискриминационного анализа.py
@@ -2,22 +2,20 @@ import numpy as np
 
 # Задание функций критериев fi(x)
 def f1(x):
-    return a1 * (x[0] - b1)**2 + c1 * (x[1] - d1)**2
+    return (x[0] - 2)**2 + 3 * (x[1] - 4)**2
 
 def f2(x):
-    return a2 * (x[0] - b2)**2 + c2 * (x[1] - d2)**2
+    return 2 * (x[0] + 3)**2 + 4 * (x[1] + 1)**2
 
 def f3(x):
-    return a3 * (x[0] - b3)**2 + c3 * (x[1] - d3)**2
+    return 3 * (x[0] + 4)**2 + 5 * (x[1] - 3)**2
 
 # Задание параметров функций
-a1, b1, c1, d1 = 1, 2, 3, 4
-a2, b2, c2, d2 = 2, -3, 4, -1
-a3, b3, c3, d3 = 3, 4, 5, -3
+a1, a2, a3 = 0.4, 0.3, 0.3
 
-# Реализация метода дискриминационного анализа
+# Реализация метода дискриминационного анализа с весами
 def discrimination_analysis(x):
-    f_values = [f1(x), f2(x), f3(x)]
+    f_values = [a1 * f1(x), a2 * f2(x), a3 * f3(x)]
     min_index = np.argmin(f_values)
     return min_index + 1
 
@@ -41,4 +39,4 @@ def find_optimal_point():
 
 # Вызов функции поиска оптимальной точки
 optimal_point = find_optimal_point()
-print("Оптимальная точка по методу дискриминационного анализа:", optimal_point)
+print("Оптимальная точка по методу дискриминационного анализа с весами:", optimal_point)
