6ece074032ea804b1bb86307b1e649221ac5a46f
Nikitpolin <124647291+Nikitpolin@users.noreply.github.com>
2024-05-04 08:18:43 +0600

    Update метод последовательных уступок.py

--- laba5/метод последовательных уступок.py
+++ laba5/метод последовательных уступок.py
@@ -2,43 +2,44 @@ import numpy as np
 
 # Задание функций критериев fi(x)
 def f1(x):
-    return (x[0] - 2)**2 + 3 * (x[1] - 4)**2
+    return a1 * (x[0] - b1)**2 + c1 * (x[1] - d1)**2
 
 def f2(x):
-    return 2 * (x[0] + 3)**2 + 4 * (x[1] + 1)**2
+    return a2 * (x[0] - b2)**2 + c2 * (x[1] - d2)**2
 
 def f3(x):
-    return 3 * (x[0] + 4)**2 + 5 * (x[1] - 3)**2
+    return a3 * (x[0] - b3)**2 + c3 * (x[1] - d3)**2
 
 # Задание параметров функций
-a1, a2, a3 = 0.4, 0.3, 0.3
+a1, b1, c1, d1 = 0.4, 2, 3, 4
+a2, b2, c2, d2 = 0.3, -3, 4, -1
+a3, b3, c3, d3 = 0.3, 4, 5, -3
 
-# Реализация метода последовательных уступок с весами
-def sequential_yield(x):
-    f_values = [a1 * f1(x), a2 * f2(x), a3 * f3(x)]
+# Реализация метода последовательных уступок
+def successive_concessions(x):
+    f_values = [f1(x), f2(x), f3(x)]
     min_index = np.argmin(f_values)
-    min_value = min(f_values)
-    for i, f in enumerate(f_values):
-        if f < min_value:
-            return None
     return min_index + 1
 
 # Поиск оптимальной точки
 def find_optimal_point():
-    # Инициализация оптимальной точки
+    # Инициализация оптимальной точки и минимального индекса
     optimal_point = None
+    min_index = np.inf
     
     # Перебор всех возможных точек
     for x1 in np.linspace(0, 10, num=100):
         for x2 in np.linspace(0, 10, num=100):
             # Вычисление индекса минимальной функции
-            index = sequential_yield((x1, x2))
-            if index is not None:
+            index = successive_concessions((x1, x2))
+            # Поиск минимального индекса
+            if index < min_index:
+                min_index = index
                 optimal_point = (x1, x2)
-                return optimal_point
     
     return optimal_point if optimal_point is not None else "No optimal point found"
 
 # Вызов функции поиска оптимальной точки
 optimal_point = find_optimal_point()
 print("Оптимальная точка по методу последовательных уступок:", optimal_point)
+
